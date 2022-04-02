5058e8eed19d4157838395ae9cfcb5e827c7f15c
juanchi097 <juanchi097@gmail.com>
2022-04-02 01:18:04 -0300

    Mint Button Aplied

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import './styles/App.css';
 import linkedinLogo from './assets/linkedin-logo.svg';
+import contractAbi from './utils/contractABI.json';
+
+import { ethers } from "ethers";
 
 // Constants
 const LINKEDIN_HANDLE = 'in/juan-cruz-yañez-075488231/';
 const LINKEDIN_LINK = `https://linkedin.com/${LINKEDIN_HANDLE}`;
 
+const tld = ".fresh";
+const CONTRACT_ADDRESS = "0x1b8241b396f1EF17934D8fAaC9E257232Ba48A2E";
+
 const App = () => {
 
 	//Just a state variable we use to store our user's public wallet. Don't forget to import useState at the top.
 	const [currentAccount, setCurrentAccount] = useState('');
+
+	const [domain, setDomain] = useState('');
+	const [record, setRecord] = useState('');
 
 	const connectWallet = async () => {
 		try {
@@ -53,7 +62,53 @@ const App = () => {
 		} else {
 			console.log('No authorized account found');
 		}
-	}
+	};
+
+	const mintDomain = async () => {
+		// Don't run if the domain is empty
+		if (!domain) { return }
+		// Alert the user if the domain is too short
+		if (domain.length < 3) {
+			alert('Domain must be at least 3 characters long');
+			return;
+		}
+		// Calculate price based on length of domain (change this to match your contract)	
+		// 3 chars = 0.5 MATIC, 4 chars = 0.3 MATIC, 5 or more = 0.1 MATIC
+		const price = domain.length === 3 ? '0.5' : domain.length === 4 ? '0.3' : '0.1';
+		console.log("Minting domain", domain, "with price", price);
+		try {
+			const { ethereum } = window;
+			if (ethereum) {
+				const provider = new ethers.providers.Web3Provider(ethereum);
+				const signer = provider.getSigner();
+				const contract = new ethers.Contract(CONTRACT_ADDRESS, contractAbi.abi, signer);
+		
+				console.log("Going to pop wallet now to pay gas...")
+				let tx = await contract.register(domain, {value: ethers.utils.parseEther(price)});
+				// Wait for the transaction to be mined
+				const receipt = await tx.wait();
+		
+				// Check if the transaction was successfully completed
+				if (receipt.status === 1) {
+					console.log("Domain minted! https://mumbai.polygonscan.com/tx/"+tx.hash);
+						
+					// Set the record for the domain
+					tx = await contract.setRecord(domain, record);
+					await tx.wait();
+		
+					console.log("Record set! https://mumbai.polygonscan.com/tx/"+tx.hash);
+						
+					setRecord('');
+					setDomain('');
+				} else {
+					alert("Transaction failed! Please try again");
+				}
+			}
+		}
+		catch(error){
+			console.log(error);
+		}
+	};
 
 	// Creating a function to render if wallet is not connected yet
 	// Create a function to render if wallet is not connected yet
@@ -65,6 +120,39 @@ const App = () => {
 		  </button>
 		</div>
 	);
+
+	const renderInputForm = () => {
+		return (
+			<div className="form-container">
+				<div className="first-row">
+					<input
+						type="text"
+						value={domain}
+						placeholder='domain'
+						onChange={e => setDomain(e.target.value)}
+					/>
+					<p className='tld'> {tld} </p>
+				</div>
+
+				<input
+					type="text"
+					value={record}
+					placeholder='something fresh'
+					onChange={e => setRecord(e.target.value)}
+				/>
+
+				<div className="button-container">
+					<button className='cta-button mint-button' disabled={null} onClick={mintDomain}>
+						Mint
+					</button>  
+					<button className='cta-button mint-button' disabled={null} onClick={null}>
+						Set data
+					</button>  
+				</div>
+
+			</div>
+		);
+	}
 
 	// This useEffect runs our function when the page loads
 	useEffect(() => {
@@ -85,6 +173,7 @@ const App = () => {
 	
 			{/* Add your render method here */}
 			{!currentAccount && renderNotConnectedContainer()}
+			{currentAccount && renderInputForm()}
 	
 			<div className="footer-container">
 			  <img alt="Linkedin Logo" className="linkedin-logo" src={linkedinLogo} />
