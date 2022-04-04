39720bb93b104b98d9eb398af662854e67eec593
juanchi097 <juanchi097@gmail.com>
2022-04-03 21:12:56 -0300

    new

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,9 @@ const App = () => {
 	const [domain, setDomain] = useState('');
 	const [record, setRecord] = useState('');
 	const [network, setNetwork] = useState('');
+	const [editing, setEditing] = useState(false);
+	const [loading, setLoading] = useState(false)
+	const [mints, setMints] = useState([]);
 
 	const connectWallet = async () => {
 		try {
@@ -152,6 +155,10 @@ const App = () => {
 					await tx.wait();
 		
 					console.log("Record set! https://mumbai.polygonscan.com/tx/"+tx.hash);
+
+					setTimeout(() => {
+						fetchMints();
+					}, 2000);
 						
 					setRecord('');
 					setDomain('');
@@ -163,6 +170,69 @@ const App = () => {
 		catch(error){
 			console.log(error);
 		}
+	};
+
+	const fetchMints = async () => {
+		try {
+		  const { ethereum } = window;
+		  if (ethereum) {
+			// You know all this
+			const provider = new ethers.providers.Web3Provider(ethereum);
+			const signer = provider.getSigner();
+			const contract = new ethers.Contract(CONTRACT_ADDRESS, contractAbi.abi, signer);
+			  
+			// Get all the domain names from our contract
+			const names = await contract.getAllNames();
+			  
+			// For each name, get the record and the address
+			const mintRecords = await Promise.all(names.map(async (name) => {
+			const mintRecord = await contract.records(name);
+			const owner = await contract.domains(name);
+			return {
+			  id: names.indexOf(name),
+			  name: name,
+			  record: mintRecord,
+			  owner: owner,
+			};
+		  }));
+	  
+		  console.log("MINTS FETCHED ", mintRecords);
+		  setMints(mintRecords);
+		  }
+		} catch(error){
+		  console.log(error);
+		}
+	};
+
+	useEffect(() => {
+		if (network === 'Polygon Mumbai Testnet') {
+		  fetchMints();
+		}
+	}, [currentAccount, network]);
+
+	const updateDomain = async () => {
+		if (!record || !domain) { return }
+		setLoading(true);
+		console.log("Updating domain", domain, "with record", record);
+		  try {
+		  const { ethereum } = window;
+		  if (ethereum) {
+			const provider = new ethers.providers.Web3Provider(ethereum);
+			const signer = provider.getSigner();
+			const contract = new ethers.Contract(CONTRACT_ADDRESS, contractAbi.abi, signer);
+	  
+			let tx = await contract.setRecord(domain, record);
+			await tx.wait();
+			console.log("Record set https://mumbai.polygonscan.com/tx/"+tx.hash);
+	  
+			fetchMints();
+			setRecord('');
+			setDomain('');
+		  }
+		  } catch(error) {
+			console.log(error);
+		  }
+		setLoading(false);
 	};
 
 	// Creating a function to render if wallet is not connected yet
@@ -187,34 +257,79 @@ const App = () => {
 		}
 		return (
 			<div className="form-container">
-				<div className="first-row">
-					<input
-						type="text"
-						value={domain}
-						placeholder='domain'
-						onChange={e => setDomain(e.target.value)}
-					/>
-					<p className='tld'> {tld} </p>
-				</div>
-
+			  <div className="first-row">
 				<input
-					type="text"
-					value={record}
-					placeholder='something fresh'
-					onChange={e => setRecord(e.target.value)}
+				  type="text"
+				  value={domain}
+				  placeholder='domain'
+				  onChange={e => setDomain(e.target.value)}
 				/>
-
-				<div className="button-container">
-					<button className='cta-button mint-button' disabled={null} onClick={mintDomain}>
-						Mint
+				<p className='tld'> {tld} </p>
+			  </div>
+	  
+			  <input
+				type="text"
+				value={record}
+				placeholder='something fresh'
+				onChange={e => setRecord(e.target.value)}
+			  />
+				{/* If the editing variable is true, return the "Set record" and "Cancel" button */}
+				{editing ? (
+				  <div className="button-container">
+					// This will call the updateDomain function we just made
+					<button className='cta-button mint-button' disabled={loading} onClick={updateDomain}>
+					  Set record
 					</button>  
-					<button className='cta-button mint-button' disabled={null} onClick={null}>
-						Set data
+					// This will let us get out of editing mode by setting editing to false
+					<button className='cta-button mint-button' onClick={() => {setEditing(false)}}>
+					  Cancel
 					</button>  
-				</div>
-
+				  </div>
+				) : (
+				  // If editing is not true, the mint button will be returned instead
+				  <button className='cta-button mint-button' disabled={loading} onClick={mintDomain}>
+					Mint
+				  </button>  
+				)}
 			</div>
 		);
+	};
+
+	const renderMints = () => {
+		if (currentAccount && mints.length > 0) {
+		  return (
+			<div className="mint-container">
+			  <p className="subtitle"> Recently minted domains!</p>
+			  <div className="mint-list">
+				{ mints.map((mint, index) => {
+				  return (
+					<div className="mint-item" key={index}>
+					  <div className='mint-row'>
+						<a className="link" href={`https://testnets.opensea.io/assets/mumbai/${CONTRACT_ADDRESS}/${mint.id}`} target="_blank" rel="noopener noreferrer">
+						  <p className="underlined">{' '}{mint.name}{tld}{' '}</p>
+						</a>
+						{/* If mint.owner is currentAccount, add an "edit" button*/}
+						{ mint.owner.toLowerCase() === currentAccount.toLowerCase() ?
+						  <button className="edit-button" onClick={() => editRecord(mint.name)}>
+							<img className="edit-icon" src="https://img.icons8.com/metro/26/000000/pencil.png" alt="Edit button" />
+						  </button>
+						  :
+						  null
+						}
+					  </div>
+				<p> {mint.record} </p>
+			  </div>)
+			  })}
+			</div>
+		  </div>);
+		}
+	};
+	  
+	// This will take us into edit mode and show us the edit buttons!
+	const editRecord = (name) => {
+		console.log("Editing record for", name);
+		setEditing(true);
+		setDomain(name);
 	}
 
 	// This useEffect runs our function when the page loads
@@ -241,6 +356,7 @@ const App = () => {
 			{/* Add your render method here */}
 			{!currentAccount && renderNotConnectedContainer()}
 			{currentAccount && renderInputForm()}
+			{mints && renderMints()}
 	
 			<div className="footer-container">
 			  <img alt="Linkedin Logo" className="linkedin-logo" src={linkedinLogo} />
